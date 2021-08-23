41d98258474c0971123876e77e825011c72e1d52
Ardian F Firmansyah <ardianfirmansyah123@gmail.com>
2021-08-23 21:34:34 +0700

    eloquent done

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,4 +13,8 @@ class Post extends Model
     //     'title','excerpt','body'
     // ];
     protected $guarded = ['id'];
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.main')
 
 @section('container')
+<h1 class="mb-5">Post Category : {{ $category }} </h1>
     @foreach ($posts as $post)
     <article class="mb-5">
-        <a href="/post/{{ $post->slug }}"><h2 class="btn btn-info">{{ $post->title }}</h2></a>
+        <a href="/post/{{ $post->slug }}"><h2 class="btn btn-info">Judul : {{ $post->title }}</h2></a>
         {{-- <h5>Author : {{ $post->author}}</h5> --}}
         <p style="text-align:justify">{!! $post->excerpt !!}</p>
     </article>
